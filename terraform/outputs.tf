90e04f30d9aefb3e22ba782a23d5cefabec4ea04
DevenEllis <dellis@515tech.com>
2026-01-06 14:50:46 -0600

    updating outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,14 +2,22 @@ output "resource_group" {
   value = azurerm_resource_group.lab.name
 }
 
+output "domain_name" {
+  value = var.domain_name
+}
+
+output "dc_vm_name" {
+  value = azurerm_windows_virtual_machine.dc.name
+}
+
 output "dc_private_ip" {
   value = azurerm_network_interface.dc.private_ip_address
 }
 
-output "iis_private_ip" {
-  value = azurerm_network_interface.iis.private_ip_address
+output "iis_vm_name" {
+  value = azurerm_windows_virtual_machine.iis.name
 }
 
-output "domain_name" {
-  value = var.domain_name
+output "iis_private_ip" {
+  value = azurerm_network_interface.iis.private_ip_address
 }
